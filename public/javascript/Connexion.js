b015bcb40aa88b102d93acbf9bcdf7e7d8201a62
tchenavier <thibaud.chenavier@gmail.com>
2026-02-20 12:46:38 +0100

    ajustement des donnée utile a conserver en local

--- public/javascript/Connexion.js
+++ public/javascript/Connexion.js
@@ -12,12 +12,7 @@ loginButton.addEventListener('click', () => {
         body: JSON.stringify({ login: loginInput, pasword: passwordInput })
     }).then(response => response.json())
         .then(data => {
-            alert(data.message);
-            alert('ID utilisateur : ' + data.user.id);
-            alert('login de utilisateur : ' + data.user.login);
-            alert('idRole utilisateur : ' + data.user.idRole);
             localStorage.setItem('userId', data.user.id);//depose l'id en localStorage
             localStorage.setItem('userLogin', data.user.login);//depose du login en localStorage
-            localStorage.setItem('userIdRole', data.user.idRole);//depose l'idRole en localStorage
         });
 });
